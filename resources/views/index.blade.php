6087667a0eb0f048b7f766a9b5f4927907308705
zeniemand <serhiy_niemand@protonmail.com>
2022-05-21 19:55:02 +0300

    project initialiazation

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MEREJA</title>
+</head>
+<body>
+<div id="app">
+
+    <example-component></example-component>
+    <h1>Mereja yo!!!</h1>
+
+</div>
+<script src="{{mix('js/app.js')}}"></script>
+</body>
+</html>
