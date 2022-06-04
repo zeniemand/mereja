fe75dffa688cf220fb87191752ecc926acc47389
zeniemand <serhiy_niemand@protonmail.com>
2022-06-04 18:40:13 +0300

    [MR-4] scss setup

--- resources/js/view/layout/Container.vue
+++ resources/js/view/layout/Container.vue
@@ -1,5 +1,5 @@
 <template>
-<h1>Vue Router Hello!!!</h1>
+<h1 class="container">Vue Router Hello!!!</h1>
 </template>
 
 <script>
@@ -8,6 +8,18 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+
+.container {
+    max-width: 120rem;
+    margin: 8rem auto;
+    background-color: var(--color-gray-light-1);
+    box-shadow: var(--shadow-dark);
+    min-height: 50rem;
+
+    @media only screen and (max-width: 75rem) {
+
+    }
+}
 
 </style>
